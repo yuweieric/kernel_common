5bbddbece9f8bf0e7b394c038efe070b4d1cec96
Jaegeuk Kim <jaegeuk@google.com>
2020-10-06 17:45:55 -0700

    ANDROID: scsi: ufs: add hooks to monitor stats of UFS traffic
    This requires for UFS stat implementations.
    
    Bug: 158050260
    Bug: 181359082
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I7b692ad1dbca1aa3f37b10d7ab91e89499bb68cd
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2041,6 +2041,7 @@ void ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag)
 	lrbp->issue_time_stamp = ktime_get();
 	lrbp->compl_time_stamp = ktime_set(0, 0);
 	ufshcd_vops_setup_xfer_req(hba, task_tag, (lrbp->cmd ? true : false));
+	trace_android_vh_ufs_send_command(hba, lrbp);
 	ufshcd_add_command_trace(hba, task_tag, UFS_CMD_SEND);
 	ufshcd_clk_scaling_start_busy(hba);
 	__set_bit(task_tag, &hba->outstanding_reqs);
@@ -5117,6 +5118,7 @@ static void __ufshcd_transfer_req_compl(struct ufs_hba *hba,
 		lrbp->compl_time_stamp = ktime_get();
 		cmd = lrbp->cmd;
 		if (cmd) {
+			trace_android_vh_ufs_compl_command(hba, lrbp);
 			ufshcd_add_command_trace(hba, index, UFS_CMD_COMP);
 			result = ufshcd_transfer_rsp_status(hba, lrbp);
 			scsi_dma_unmap(cmd);
